b3f1ec05b15660e0957075441561d3a23fdcbd00
AISoftTech <idreesfaiz9@gmail.com>
2022-12-15 23:11:42 +0500

    M Zubair 2010 Backend

---
+++ Department_Backend/DPT_Cruds.sql
@@ -0,0 +1,8 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00"; 
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+ 
